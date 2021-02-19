9da553a47fc73eb750394852b00d39043d84faa7
Guillaume Hertault <guihash@guillaumehertault.fr>
2021-02-19 11:09:58 +0100

    fix: better error manager
    better typing on lacrosse api

--- src/lacrosse.ts
+++ src/lacrosse.ts
@@ -14,43 +14,95 @@ interface ResponseData<T> {
 }
 
 export type DeviceWeatherData = {
-  humidity: number
-  temperature: number
-  barometricPressure: number
-  heatIndex: number
+  humidity?: number
+  temperature?: number
+  barometricPressure?: number
+  heatIndex?: number
 }
 
-// TODO complete types
-
-interface ResponseLogin {
+type ResponseLogin = {
   idToken: string
-}
-
-type Sensor = {
-  fields: [string]
+  expiresIn: string
+  kind: string
+  localId: string
+  displayName?: string
+  registered: boolean
+  refreshToken: string
 }
 
 type Device = {
   id: string
+  modifiedOn: Date
   name: string
   sensor: Sensor
+  createdOn: Date
+  shallow: boolean
+  weight: string
+  flaggedForSynchVNext: boolean
+  sensorId: string
+  locationId: string
+  ownerId: string
+}
+
+// TODO complete types
+type Sensor = {
+  fields: [string]
 }
 
 type Location = {
   id: string
 }
 
-type RawWeatherData = Record<string, unknown>
+enum Unit {
+  Celcius = 'degrees_celsius',
+  Fahrenheit = 'degrees_fahrenheit',
+  RelativeHumidity = 'relative_humidity',
+  Millibars = 'millibars',
+}
+
+type DataField = {
+  values: [{ u: number; s: number }]
+  unit_enum: number
+  unit: Unit
+}
+
+type RawWeatherData = {
+  [key: string]: {
+    [key: string]: {
+      time_zone: string
+      range: {
+        unix: { to: number; from: number; continue: number }
+        iso8601: {
+          to: Date
+          from: Date
+          continue: Date
+        }
+      }
+      fields: {
+        Temperature?: DataField
+        Humidity?: DataField
+        HeatIndex?: DataField
+        BarometricPressure?: DataField
+      }
+    }
+  }
+}
 
 export default class LaCrosseAPI {
-  private token: string
   private email: string
   private password: string
+  private token: { expiresAt: number; value: string }
 
   constructor(email: string, password: string) {
     this.email = email
     this.password = password
-    this.token = ''
+    this.token = { expiresAt: Date.now(), value: '' }
+  }
+
+  private async renewTokenIfNeeded() {
+    if (Date.now() > this.token.expiresAt) {
+      await this.login()
+    }
   }
 
   private async login() {
@@ -61,39 +113,33 @@ export default class LaCrosseAPI {
     }
 
     // TODO manage 400 errors
-    const body: ResponseLogin = await fetch(LACROSSE_LOGIN_URL, {
+    const { idToken: value, expiresIn }: ResponseLogin = await fetch(LACROSSE_LOGIN_URL, {
       json,
       method: 'POST',
     })
 
-    const token = body.idToken
-
-    if (!token) {
+    if (!value) {
       throw new Error('Login Failed. Check credentials and try again')
     }
 
-    this.token = token
+    this.token = { value, expiresAt: Date.now() + Number(expiresIn) * 1000 - 1000 }
   }
 
   async getLocations(): Promise<Location[]> {
-    if (!this.token) {
-      await this.login()
-    }
-    const body: ResponseData<Location> = await fetch(LACROSSE_LOCATION_URL, undefined, this.token)
+    await this.renewTokenIfNeeded()
+    const body: ResponseData<Location> = await fetch(LACROSSE_LOCATION_URL, undefined, this.token.value)
 
     return body.items
   }
 
   async getDevices(locations?: Location[]): Promise<Device[]> {
-    if (!this.token) {
-      await this.login()
-    }
+    await this.renewTokenIfNeeded()
     if (!locations) {
       locations = await this.getLocations()
     }
     for (const location of locations) {
       const url = LACROSSE_DEVICES_URL.replace('%ID%', location.id)
-      const body: ResponseData<Device> = await fetch(url, undefined, this.token)
+      const body: ResponseData<Device> = await fetch(url, undefined, this.token.value)
 
       // TODO manage multiple locations ?
       return body.items
@@ -103,18 +149,16 @@ export default class LaCrosseAPI {
   }
 
   async getDeviceWeatherData(device: Device): Promise<DeviceWeatherData> {
-    if (!this.token) {
-      await this.login()
-    }
+    await this.renewTokenIfNeeded()
 
     const url = LACROSSE_WEATHER_URL.replace('%DEVICE_ID%', device.id)
     const tz = 'Europe/Paris'
     const fields = Object.keys(device.sensor.fields).join()
 
-    // Data is updated each 200000ms and we want only the last update
+    // Data is updated each 200000ms on the api and we want only the last update
     const from = Date.now() - 200000
 
-    const data: ResponseData<RawWeatherData> = await fetch(
+    const data: RawWeatherData = await fetch(
       url,
       {
         searchParams: {
@@ -125,33 +169,47 @@ export default class LaCrosseAPI {
           types: 'spot',
         },
       },
-      this.token,
+      this.token.value,
     )
 
-    const dataFields = data[`ref.user-device.${device.id}`]['ai.ticks.1'].fields
+    const dataFields = data[`ref.user-device.${device.id}`]?.['ai.ticks.1']?.fields
 
-    const valuesTemperature = dataFields.Temperature?.values
-    const valuesHunidity = dataFields.Humidity?.values
-    const valuesHeatIndex = dataFields.HeatIndex?.values
-    const valuesBarometricPressure = dataFields.BarometricPressure?.values
-
-    const lastTemperature = valuesTemperature?.pop().s
-    const lastHumidity = valuesHunidity?.pop().s
-    const lastHeatIndex = valuesHeatIndex?.pop().s
-    const lastBarometricPressure = valuesBarometricPressure?.pop().s
+    const valuesTemperature = convertToCelcius(dataFields?.Temperature)?.values
+    const valuesHunidity = dataFields?.Humidity?.values
+    const valuesHeatIndex = convertToCelcius(dataFields?.HeatIndex)?.values
+    const valuesBarometricPressure = dataFields?.BarometricPressure?.values
 
     return {
-      humidity: lastHumidity,
-      temperature: lastTemperature,
-      barometricPressure: lastBarometricPressure,
-      heatIndex: lastHeatIndex,
+      humidity: getLastValue(valuesHunidity),
+      temperature: getLastValue(valuesTemperature),
+      barometricPressure: getLastValue(valuesBarometricPressure),
+      heatIndex: getLastValue(valuesHeatIndex),
     }
   }
 }
 
+function getLastValue(values?: { u: number; s: number }[]): number | undefined {
+  return values?.pop()?.s
+}
+
+function convertToCelcius(temperature?): DataField {
+  if (!temperature) return temperature
+  if (temperature.unit === Unit.Celcius) {
+    return temperature
+  }
+  if (temperature.unit === Unit.Fahrenheit) {
+    temperature.values = temperature.values.map(({ u, s }) => ({ u, s: (s - 32) * (5 / 9) }))
+
+    return temperature
+  }
+
+  throw new Error(`Unit ${temperature.unit} cannot be converted to celcius`)
+}
+
 interface fetch {
-  <T>(url: string | URL, options: Options | undefined, token: string): Promise<ResponseData<T>>
   (url: string | URL, options: Options): Promise<ResponseLogin>
+  (url: string | URL, options: Options, token: string): Promise<RawWeatherData>
+  <T>(url: string | URL, options: Options | undefined, token: string): Promise<ResponseData<T>>
 }
 
 const fetch: fetch = async function <T>(url, options?, token?): Promise<T> {
@@ -159,12 +217,16 @@ const fetch: fetch = async function <T>(url, options?, token?): Promise<T> {
 
   const { searchParams, json, method } = options || {}
 
-  return got(url, {
-    method,
-    resolveBodyOnly: true,
-    responseType: 'json',
-    headers,
-    searchParams,
-    json,
-  })
+  try {
+    return await got(url, {
+      method,
+      resolveBodyOnly: true,
+      responseType: 'json',
+      headers,
+      searchParams,
+      json,
+    })
+  } catch (e) {
+    throw new Error(e.response?.body?.error?.message || e.message)
+  }
 }
