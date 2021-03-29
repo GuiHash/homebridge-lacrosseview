52d367acaace4286da687c395a93fa911edb8e56
Guillaume Hertault <guihash@guillaumehertault.fr>
2021-03-29 19:51:50 +0200

    fix: create only necessary service on accessories

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -9,8 +9,8 @@ import { LaCrosseViewPlatform } from './platform'
  * Each accessory may expose multiple services of different service types.
  */
 export class Accessory {
-  private service: Service
-  private humiditySensorService: Service
+  private temperatureSensorService?: Service
+  private humiditySensorService?: Service
   private lacrosse: LaCrosseAPI
   private log: Logger
 
@@ -25,21 +25,25 @@ export class Accessory {
       .setCharacteristic(platform.Characteristic.Model, accessory.context?.device.sensor.type.name)
       .setCharacteristic(platform.Characteristic.SerialNumber, accessory.context?.device.sensor.serial)
 
-    // set service temperature
-    this.service =
-      accessory.getService(platform.Service.TemperatureSensor) ||
-      accessory.addService(platform.Service.TemperatureSensor)
+    if (isTemperatureAccessory(accessory)) {
+      // set service temperature
+      this.temperatureSensorService =
+        accessory.getService(platform.Service.TemperatureSensor) ||
+        accessory.addService(platform.Service.TemperatureSensor)
 
-    this.service.setCharacteristic(platform.Characteristic.Name, accessory.displayName)
-    this.service.setCharacteristic(platform.Characteristic.StatusActive, 0)
+      this.temperatureSensorService.setCharacteristic(platform.Characteristic.Name, accessory.displayName)
+      this.temperatureSensorService.setCharacteristic(platform.Characteristic.StatusActive, 0)
+    }
 
-    // set service humidity
-    this.humiditySensorService =
-      accessory.getService(platform.Service.HumiditySensor) ||
-      this.accessory.addService(platform.Service.HumiditySensor)
+    if (isHumidityAccessory(accessory)) {
+      // set service humidity
+      this.humiditySensorService =
+        accessory.getService(platform.Service.HumiditySensor) ||
+        this.accessory.addService(platform.Service.HumiditySensor)
 
-    this.humiditySensorService.setCharacteristic(platform.Characteristic.Name, accessory.displayName)
-    this.humiditySensorService.setCharacteristic(platform.Characteristic.StatusActive, 0)
+      this.humiditySensorService.setCharacteristic(platform.Characteristic.Name, accessory.displayName)
+      this.humiditySensorService.setCharacteristic(platform.Characteristic.StatusActive, 0)
+    }
 
     this.updateDataSensors()
     this.log.debug(`[%s] fireCharacteristicUpdateInterval [%s]`, accessory.displayName, platform.config.pollingInterval)
@@ -54,21 +58,37 @@ export class Accessory {
 
       const { humidity, temperature } = await this.lacrosse.getDeviceWeatherData(this.accessory.context.device)
 
-      if (humidity) {
+      if (humidity && this.humiditySensorService) {
         this.humiditySensorService.updateCharacteristic(this.platform.Characteristic.StatusActive, 1)
         this.humiditySensorService.updateCharacteristic(this.platform.Characteristic.CurrentRelativeHumidity, humidity)
         this.log.debug(`[%s] updateCharacteristic [%s] Humidity`, this.accessory.displayName, humidity)
       }
 
-      if (temperature) {
-        this.service.updateCharacteristic(this.platform.Characteristic.StatusActive, 1)
-        this.service.updateCharacteristic(this.platform.Characteristic.CurrentTemperature, temperature)
+      if (temperature && this.temperatureSensorService) {
+        this.temperatureSensorService.updateCharacteristic(this.platform.Characteristic.StatusActive, 1)
+        this.temperatureSensorService.updateCharacteristic(this.platform.Characteristic.CurrentTemperature, temperature)
         this.log.debug(`[%s] updateCharacteristic [%s] Temperature`, this.accessory.displayName, temperature)
       }
     } catch (e) {
-      this.humiditySensorService.updateCharacteristic(this.platform.Characteristic.StatusActive, 0)
-      this.service.updateCharacteristic(this.platform.Characteristic.StatusActive, 0)
       this.log.error('[%s] %s', this.accessory.displayName, e)
+      if (this.humiditySensorService) {
+        this.humiditySensorService.updateCharacteristic(this.platform.Characteristic.StatusActive, 0)
+      }
+      if (this.temperatureSensorService) {
+        this.temperatureSensorService.updateCharacteristic(this.platform.Characteristic.StatusActive, 0)
+      }
     }
   }
+}
+
+function isTemperatureAccessory(accessory) {
+  return typeof getFields(accessory).Temperature === 'number'
+}
+
+function isHumidityAccessory(accessory) {
+  return typeof getFields(accessory).Humidity === 'number'
+}
+
+function getFields(accessory) {
+  return accessory.context.device.sensor.fields
 }
